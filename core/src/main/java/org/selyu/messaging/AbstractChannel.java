8c62e5ec8bfd9639c541e82f00d81a02930b384b
Lilian <lilyarmes@protonmail.com>
2020-04-29 12:45:45 -0400

    Close RabbitMQ connection with shutdown method

--- core/src/main/java/org/selyu/messaging/AbstractChannel.java
+++ core/src/main/java/org/selyu/messaging/AbstractChannel.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractChannel implements IChannel {
     protected final Gson gson;
--- rabbitmq/src/main/java/org/selyu/messaging/impl/RabbitMQChannel.java
+++ rabbitmq/src/main/java/org/selyu/messaging/impl/RabbitMQChannel.java
@@ -17,12 +17,13 @@ import java.util.concurrent.TimeoutException;
 public final class RabbitMQChannel extends AbstractChannel {
     protected final String rabbitChannel;
     private final Channel channel;
+    private final Connection connection;
 
     public RabbitMQChannel(@NotNull ConnectionFactory factory, @NotNull String rabbitChannel, @Nullable Gson gson) throws IOException, TimeoutException {
         super(gson == null ? new Gson() : gson);
         this.rabbitChannel = rabbitChannel;
 
-        Connection connection = factory.newConnection();
+        connection = factory.newConnection();
         channel = connection.createChannel();
         channel.queueDeclare(rabbitChannel, false, false, false, null);
         channel.basicConsume(rabbitChannel, true, (s, delivery) -> {
@@ -36,5 +37,15 @@ public final class RabbitMQChannel extends AbstractChannel {
     @Override
     public IQueue getQueue(@NotNull String name) {
         return queues.computeIfAbsent(name, queue -> new RabbitMQQueue(this, channel, queue));
+    }
+
+    @Override
+    public void shutdown() {
+        try {
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.shutdown();
     }
 }
