7b058929fa94e7ef4c4fd654eb700a63a4ecb48e
Lillian Armes <lilyarmes@protonmail.com>
2020-07-25 20:54:12 -0400

    Use a CountDownLatch instead of Thread#sleep and make tests meaningful

--- rabbitmq/src/test/java/org/selyu/messenger/RabbitMQTest.java
+++ rabbitmq/src/test/java/org/selyu/messenger/RabbitMQTest.java
@@ -2,32 +2,53 @@ package org.selyu.messenger;
 
 import com.github.fridujo.rabbitmq.mock.MockConnectionFactory;
 import com.rabbitmq.client.ConnectionFactory;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.selyu.messaging.annotation.Subscribe;
 import org.selyu.messaging.impl.RabbitMQMessageHandler;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static org.junit.Assert.assertTrue;
+
 public final class RabbitMQTest {
+    private ConnectionFactory connectionFactory;
+    private RabbitMQMessageHandler messageHandler;
+    private CountDownLatch countDownLatch;
+
+    @Before
+    public void before() throws IOException, TimeoutException {
+        connectionFactory = new MockConnectionFactory();
+        messageHandler = new RabbitMQMessageHandler(connectionFactory, "test", null);
+        countDownLatch = new CountDownLatch(1);
+    }
+
     @Test
-    public void run() throws IOException, TimeoutException, InterruptedException {
-        ConnectionFactory factory = new MockConnectionFactory();
-        RabbitMQMessageHandler channel = new RabbitMQMessageHandler(factory, "test", null);
-        channel.subscribe(new Object() {
+    public void run() throws InterruptedException {
+        boolean[] received = {false};
+        messageHandler.subscribe(new Object() {
             @Subscribe
             public void hello(String string) {
-                System.out.println(string);
+                received[0] = true;
+                countDownLatch.countDown();
             }
 
             @Subscribe("NOT_ALL")
-            public void goodbye(String string) throws Exception{
-                System.out.println("GOODBYE: " + string);
+            public void goodbye(String string) throws Exception {
                 throw new Exception("I shouldn't receive this!");
             }
         });
-        channel.getPublisher().post("Hello World! from RabbitMQ");
+        messageHandler.getPublisher().post("Hello World!");
+        countDownLatch.await(1, TimeUnit.SECONDS);
+        assertTrue(received[0]);
+    }
 
-        Thread.sleep(100);
+    @After
+    public void after() {
+        messageHandler.shutdown();
     }
 }
--- redis-jedis/src/test/java/org/selyu/messaging/JedisTest.java
+++ redis-jedis/src/test/java/org/selyu/messaging/JedisTest.java
@@ -2,45 +2,64 @@ package org.selyu.messaging;
 
 import com.github.fppt.jedismock.RedisServer;
 import com.github.fppt.jedismock.server.ServiceOptions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.selyu.messaging.annotation.Subscribe;
 import org.selyu.messaging.impl.JedisMessageHandler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public final class JedisTest {
     private static int MOCK_PORT = 9999;
 
-    @Test
-    public void run() throws IOException, URISyntaxException, InterruptedException {
-        RedisServer server = RedisServer.newRedisServer(MOCK_PORT);
-        ServiceOptions options = new ServiceOptions() {
+    private RedisServer server;
+    private JedisMessageHandler jedisMessageHandler;
+    private CountDownLatch countDownLatch;
+
+    @Before
+    public void before() throws IOException, URISyntaxException {
+        server = RedisServer.newRedisServer(MOCK_PORT);
+        server.setOptions(new ServiceOptions() {
             @Override
             public int autoCloseOn() {
                 return 3;
             }
-        };
-        server.setOptions(options);
+        });
         server.start();
 
-        IMessageHandler channel = new JedisMessageHandler("redis://127.0.0.1:9999/0", "test", null);
-        channel.subscribe(new Object() {
+        jedisMessageHandler = new JedisMessageHandler(String.format("redis://127.0.0.1:%s/0", MOCK_PORT), "test", null);
+        countDownLatch = new CountDownLatch(1);
+    }
+
+    @Test
+    public void run() throws InterruptedException {
+        boolean[] received = new boolean[]{false};
+        jedisMessageHandler.subscribe(new Object() {
             @Subscribe
             public void helloWorld(String string) {
-                System.out.println(string);
+                received[0] = true;
+                countDownLatch.countDown();
             }
 
             @Subscribe("NOT_ALL")
             public void goodbyeWorld(String string) throws Exception {
-                System.out.println("GOODBYE: " + string);
                 throw new Exception("I should not receive this!");
             }
         });
-        channel.getPublisher().post("Hello World! from Jedis");
-        Thread.sleep(250);
+        jedisMessageHandler.getPublisher().post("Hello World! from Jedis");
+        countDownLatch.await(1, TimeUnit.SECONDS);
+        assertTrue(received[0]);
+    }
 
-        channel.shutdown();
+    @After
+    public void after() {
+        jedisMessageHandler.shutdown();
         server.stop();
     }
 }
