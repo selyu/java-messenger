24e8a267c1c027cc613110492134c8ec20bb9d14
Lillian Armes <lilyarmes@protonmail.com>
2020-07-29 03:55:35 -0400

    Add redisson & unit test

--- redis-redisson/src/test/java/RedissonTest.java
+++ redis-redisson/src/test/java/RedissonTest.java
@@ -1,44 +1,48 @@
-package org.selyu.messenger.api;
-
 import com.github.fppt.jedismock.RedisServer;
 import com.github.fppt.jedismock.server.ServiceOptions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.RedissonNodeConfig;
 import org.selyu.messenger.api.annotation.Subscribe;
-import org.selyu.messenger.redis.JedisMessageHandler;
+import org.selyu.messenger.redis.RedissonMessageHandler;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
 
-public final class JedisTest {
-    private RedisServer server;
-    private JedisMessageHandler jedisMessageHandler;
+public class RedissonTest {
+    private RedisServer redisServer;
+    private RedissonMessageHandler redissonMessageHandler;
     private CountDownLatch countDownLatch;
 
     @Before
-    public void before() throws IOException, URISyntaxException {
-        server = RedisServer.newRedisServer(9999);
-        server.setOptions(new ServiceOptions() {
+    public void before() throws IOException {
+        redisServer = RedisServer.newRedisServer(9999);
+        redisServer.setOptions(new ServiceOptions() {
             @Override
             public int autoCloseOn() {
                 return 3;
             }
         });
-        server.start();
+        redisServer.start();
 
-        jedisMessageHandler = new JedisMessageHandler(String.format("redis://127.0.0.1:%s/0", 9999), "test", null);
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:" + 9999);
+
+        redissonMessageHandler = new RedissonMessageHandler(Redisson.create(config), "test", null);
         countDownLatch = new CountDownLatch(1);
     }
 
     @Test
     public void run() throws InterruptedException {
         boolean[] received = new boolean[]{false};
-        jedisMessageHandler.subscribe(new Object() {
+        redissonMessageHandler.subscribe(new Object() {
             @Subscribe
             public void helloWorld(String string) {
                 received[0] = true;
@@ -50,14 +54,13 @@ public final class JedisTest {
                 throw new Exception("I should not receive this!");
             }
         });
-        jedisMessageHandler.getPublisher().post("Hello World! from Jedis");
+        redissonMessageHandler.getPublisher().post("Hello World! from Redisson");
         countDownLatch.await(5, TimeUnit.SECONDS);
         assertTrue(received[0]);
     }
 
     @After
     public void after() {
-        jedisMessageHandler.shutdown();
-        server.stop();
+        redisServer.stop();
     }
 }
